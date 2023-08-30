cede63e6e42352b469647f7e1f2528279f24063c
Alexandr Ergheledji <alexandr.ergheledji@mni.thm.de>
2023-08-30 11:27:24 +0200

    created the model to store and worck with modules

---
+++ src/app/module/module.model.ts
@@ -0,0 +1,12 @@
+export class Module {
+
+    public code: string;
+    public name: string;
+    public crp: number;
+
+    constructor(code: string, name: string, crp: number) {
+        this.code = code;
+        this.name = name;
+        this.crp = crp;
+    }
+}
