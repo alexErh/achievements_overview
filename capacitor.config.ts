798a11809e60ff270db6a3a129dcc10c4c66a727
Alexandr Ergheledji <alexandr.ergheledji@mni.thm.de>
2023-08-30 10:43:45 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'achievements_overview',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
